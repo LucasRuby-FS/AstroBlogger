20056eec925b5529b40faa597921f2250862b321
LucasRuby-FS <rklucas@student.fullsail.edu>
2026-02-17 23:39:20 -0500

    Updated.

--- AstroblogNew/astro.config.mjs
+++ AstroblogNew/astro.config.mjs
@@ -1,5 +1,8 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://LucasRuby-FS.github.io/AstroBlogger/",
+  base: "/AstroBlogger/",
+});
