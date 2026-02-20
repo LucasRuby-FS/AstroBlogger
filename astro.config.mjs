2d408717ad376197529ef1e754217233b8a95155
LucasRuby-FS <rklucas@student.fullsail.edu>
2026-02-19 20:04:36 -0500

    Fixing.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://LucasRuby-FS.github.io/AstroBlogger/",
-  base: "/",
+  base: "/AstroBlogger",
 });
